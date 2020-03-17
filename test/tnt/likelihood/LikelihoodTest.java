ed31afb383b00100f9581fd7dba34fa2ed4eca13
Ugne Stolz <jugne@bs-mbpr364.d.ethz.ch>
2020-03-17 10:12:23 +0100

    Before changes

--- test/tnt/likelihood/LikelihoodTest.java
+++ test/tnt/likelihood/LikelihoodTest.java
@@ -28,6 +28,7 @@ public class LikelihoodTest {
 
 	String speciesTreeNewick = "((t2_1:0.674025510498478,t7_1:0.674025510498478):1.02026915570423,((t6_1:0.441898780191674,(t11_1:0.321415277712137,t10_1:0.321415277712137):0.120483502479536):0.747685332950928,t22_1:0):0.504710553060102):2.13804639428371;";
 
+
 	@Test
 	public void topologyDistribution() throws Exception {
 
@@ -56,15 +57,15 @@ public class LikelihoodTest {
 				true);
 
 		TraitSet sampleCounts = new TraitSet();
-		sampleCounts.initByName("traitname", "sampleCounts", "taxa", taxonSet, "value", " t7_1=10,\n" +
-				"t10_1=10,\n" +
-				"t11_1=10,\n" +
-				"t6_1=10,\n" +
-				"t2_1=10,\n" +
-				"t22_1=10");
+		sampleCounts.initByName("traitname", "sampleCounts", "taxa", taxonSet, "value", " t7_1=4,\n" +
+				"t10_1=1,\n" +
+				"t11_1=1,\n" +
+				"t6_1=1,\n" +
+				"t2_1=1,\n" +
+				"t22_1=1");
 
 		RealParameter populationSizes = new RealParameter("1.0");
-		RealParameter bottleneckStrength = new RealParameter("4.0");
+		RealParameter bottleneckStrength = new RealParameter("1.0");
 
 		RealParameter birthRate = new RealParameter("1.0");
 		RealParameter deathRate = new RealParameter("0.4");
@@ -83,7 +84,7 @@ public class LikelihoodTest {
 		GeneTreeIntervals intervals = new GeneTreeIntervals();
 		intervals.initByName("simulatedGeneTree", geneTree, "transmissionTreeInput", transmissionTreeInput);
 
-		Integer runs = 100000;
+		Integer runs = 10000000;
 		Integer burnin = 0;
 		Integer logEvery = 1;
 
@@ -157,8 +158,10 @@ public class LikelihoodTest {
 		double psi;
 
 		int s;
+		int kl;
 
 		List<Double> derivatives = new ArrayList<Double>();
+		List<Double> derivatives_2 = new ArrayList<Double>();
 //		int[] endLineages = new int[6];
 
 		@Override
@@ -184,6 +187,7 @@ public class LikelihoodTest {
 			psi = samplingRateInput.get().getArrayValue(0);
 
 			s = 0;
+			kl = 1;
 
 		}
 
@@ -204,8 +208,10 @@ public class LikelihoodTest {
 			SimulatedGeneTree tree = (SimulatedGeneTree) geneTree.getCurrent();
 			intervals.requiresRecalculation();
 			HashMap<Node, List<GeneTreeEvent>> eventList = intervals.getGeneTreeEventList();
-			double h = 0.000001;
+			double h = 0.0001;
 			double derivative = 0;
+			double derivative_1 = 1;
+			double derivative_2 = 1;
 
 //			System.out.println(geneTree.getRoot().toNewick());
 
@@ -214,7 +220,7 @@ public class LikelihoodTest {
 					.collect(Collectors.toList());
 
 
-			Node trNode = trNodeList.get(5);
+			Node trNode = trNodeList.get(0);
 			boolean donor = (trNode.getParent().getChild(0) == trNode && !trNode.getParent().isFake());
 
 			List<GeneTreeEvent> localEventList = eventList.get(trNode).stream()
@@ -223,13 +229,18 @@ public class LikelihoodTest {
 			GeneTreeEvent prevEvent = new GeneTreeEvent();
 			prevEvent.time = trNode.getHeight();
 			for (GeneTreeEvent event : localEventList) {
+				if (event.lineages == 0)
+					System.out.println(geneTree.getRoot().toNewick());
 //				if (trNode.getParent().getHeight() - 0.001 <= event.time)
 //					break;
 
 				// Contribution from every interval, except the last
 				if (prevEvent.time < event.time) {
-					derivative += logInterval(event, prevEvent, Ne + h);
-					derivative -= logInterval(event, prevEvent, Ne);
+//					derivative_1 *= logInterval(event, prevEvent, Ne + h);
+//					derivative_2 *= logInterval(event, prevEvent, Ne - h);
+					derivative += derivativeNeLogInt(event, prevEvent);
+//					derivative += Math.log(logInterval(event, prevEvent, Ne + h));
+//					derivative -= Math.log(logInterval(event, prevEvent, Ne - h));
 				}
 
 				switch (event.type) {
@@ -241,29 +252,46 @@ public class LikelihoodTest {
 					// log as transmission event.
 					// This means no multiplication with rate \lambda*P_0
 					if (!trNode.isRoot() && !donor && event.time == trNode.getParent().getHeight()) {
-						derivative += logTrans(event, prevEvent, Ne + h);
-						derivative -= logTrans(event, prevEvent, Ne);
+//						derivative_1 *= logTrans(event, prevEvent, Ne + h);
+//						derivative_2 *= logTrans(event, prevEvent, Ne - h);
+						derivative += derivativeNeLogMulti(event, prevEvent);
+//						derivative += Math.log(logTrans(event, prevEvent, Ne + h));
+//						derivative -= Math.log(logTrans(event, prevEvent, Ne - h));
 						break;
 					}
-					derivative += logBif(event, prevEvent, Ne + h);
-					derivative -= logBif(event, prevEvent, Ne);
+//					derivative_1 *= logBif(event, prevEvent, Ne + h);
+//					derivative_2 *= logBif(event, prevEvent, Ne - h);
+					derivative += derivativeNeLogBif(event, prevEvent);
+//					derivative += Math.log(logBif(event, prevEvent, Ne + h));
+//					derivative -= Math.log(logBif(event, prevEvent, Ne - h));
 					break;
 				case MULTIFURCATION:
 					// To see how many multiple mergers we have so far
-					if (event.multiCoalSize.size() > 1)
-						System.out.println(++s);
+//					if (event.multiCoalSize.size() > 1) {
+//						System.out.println(++s);
+//						if (s == 3)
+//							System.out.println();
+////						if (s == 6373)
+////							System.out.println(geneTree.getRoot().toNewick());
+//					}
 
 					// If event is recorded at time of transmission and
 					// transmission tree branch is recipient,
 					// log as transmission event.
 					// This means no multiplication with rate \lambda*P_0
 					if (!trNode.isRoot() && !donor && event.time == trNode.getParent().getHeight()) {
-						derivative += logTrans(event, prevEvent, Ne + h);
-						derivative -= logTrans(event, prevEvent, Ne);
+//						derivative_1 *= logTrans(event, prevEvent, Ne + h);
+//						derivative_2 *= logTrans(event, prevEvent, Ne - h);
+						derivative += derivativeNeLogMulti(event, prevEvent);
+//						derivative += Math.log(logTrans(event, prevEvent, Ne + h));
+//						derivative -= Math.log(logTrans(event, prevEvent, Ne - h));
 							break;
 						}
-					derivative += logMultif(event, prevEvent, Ne + h);
-					derivative -= logMultif(event, prevEvent, Ne);
+//					derivative_1 *= logMultif(event, prevEvent, Ne + h);
+//					derivative_2 *= logMultif(event, prevEvent, Ne - h);
+					derivative += derivativeNeLogMulti(event, prevEvent);
+//					derivative += Math.log(logMultif(event, prevEvent, Ne + h));
+//					derivative -= Math.log(logMultif(event, prevEvent, Ne - h));
 				}
 				prevEvent = event;
 			}
@@ -272,14 +300,103 @@ public class LikelihoodTest {
 						GeneTreeEvent mockEvent = new GeneTreeEvent();
 				mockEvent.time = trNode.getParent().getHeight();// - 0.001;
 				mockEvent.lineages = prevEvent.lineages;
-				derivative += logInterval(mockEvent, prevEvent, Ne + h);
-				derivative -= logInterval(mockEvent, prevEvent, Ne);
-//				if (!donor) {
-//					derivative += logTrans(mockEvent, prevEvent, Ne + h);
-//					derivative -= logTrans(mockEvent, prevEvent, Ne);
-//				}
+//				derivative_1 *= logInterval(mockEvent, prevEvent, Ne + h);
+//				derivative_2 *= logInterval(mockEvent, prevEvent, Ne - h);
+				derivative += derivativeNeLogInt(mockEvent, prevEvent);
+//				derivative += Math.log(logInterval(mockEvent, prevEvent, Ne + h));
+//				derivative -= Math.log(logInterval(mockEvent, prevEvent, Ne - h));
+				if (!donor) {
+//					derivative_1 *= logTrans(mockEvent, prevEvent, Ne + h);
+//					derivative_2 *= logTrans(mockEvent, prevEvent, Ne - h);
+					derivative += derivativeNeLogMulti(mockEvent, prevEvent);
+//					derivative += Math.log(logTrans(mockEvent, prevEvent, Ne + h));
+//					derivative -= Math.log(logTrans(mockEvent, prevEvent, Ne - h));
+				}
 			}
-			derivatives.add(derivative / h);
+//			double m = Math.log(derivative_1) - Math.log(derivative_2);
+//			m /= (2 * h);
+////			derivatives.add(derivative / (2 * h));
+//			derivatives.add(m);
+
+			derivatives_2.add(derivative);
+
+			if (derivatives_2.size() == 100) {
+//				double derivatives_100 = derivatives.subList(0, 100).stream()
+//						.mapToDouble(a -> a)
+//						.sum();
+
+				double derivatives_100_2 = derivatives_2.subList(0, 100).stream()
+						.mapToDouble(a -> a)
+						.sum();
+
+//				System.out.println("100 runs: " + derivatives_100 / 100);
+				System.out.println("100 runs: " + derivatives_100_2 / 100);
+			}
+
+			if (derivatives_2.size() == 1000) {
+//				double derivatives_1000 = derivatives.subList(0, 1000).stream()
+//						.mapToDouble(a -> a)
+//						.sum();
+
+				double derivatives_1000_2 = derivatives_2.subList(0, 1000).stream()
+						.mapToDouble(a -> a)
+						.sum();
+
+//				System.out.println("1000 runs: " + derivatives_1000 / 1000);
+				System.out.println("1000 runs: " + derivatives_1000_2 / 1000);
+			}
+
+			if (derivatives_2.size() / 10000.0 == derivatives_2.size() / 10000 && derivatives_2.size() / 10000 >= 1
+					&& derivatives_2.size() / 10000 < 10) {
+//				double derivatives_10000 = derivatives.subList(0, kl * 10000).stream()
+//						.mapToDouble(a -> a)
+//						.sum();
+
+				double derivatives_10000_2 = derivatives_2.subList(0, kl * 10000).stream()
+						.mapToDouble(a -> a)
+						.sum();
+
+//				System.out.println("10000 runs: " + derivatives_10000 / (kl * 10000));
+				System.out.println(kl + "x 10000 runs: " + derivatives_10000_2 / (kl * 10000));
+				kl++;
+			}
+
+			if (derivatives_2.size() == 100000) {
+//				double derivatives_100000 = derivatives.subList(0, 100000).stream()
+//						.mapToDouble(a -> a)
+//						.sum();
+				double derivatives_100000_2 = derivatives_2.subList(0, 100000).stream()
+						.mapToDouble(a -> a)
+						.sum();
+
+//				System.out.println("100000 runs: " + derivatives_100000 / 100000);
+				System.out.println("100000 runs: " + derivatives_100000_2 / 100000);
+			}
+
+			if (derivatives_2.size() == 1000000) {
+//				double derivatives_1000000 = derivatives.subList(0, 1000000).stream()
+//						.mapToDouble(a -> a)
+//						.sum();
+				double derivatives_1000000_2 = derivatives_2.subList(0, 1000000).stream()
+						.mapToDouble(a -> a)
+						.sum();
+
+//				System.out.println("1000000 runs: " + derivatives_1000000 / 1000000);
+				System.out.println("1000000 runs: " + derivatives_1000000_2 / 1000000);
+			}
+
+			if (derivatives_2.size() == 10000000) {
+//				double derivatives_10000000 = derivatives.subList(0, 1000000).stream()
+//						.mapToDouble(a -> a)
+//						.sum();
+				double derivatives_10000000_2 = derivatives_2.subList(0, 1000000).stream()
+						.mapToDouble(a -> a)
+						.sum();
+
+//				System.out.println("10000000 runs: " + derivatives_10000000 / 10000000);
+				System.out.println("10000000 runs: " + derivatives_10000000_2 / 10000000);
+			}
+
 		}
 
 		public List<Double> getAnalysis() {
@@ -292,19 +409,50 @@ public class LikelihoodTest {
 			double derivatives_100 = derivatives.subList(0, 100).stream()
 					.mapToDouble(a -> a)
 					.sum();
+
+			double derivatives_100_2 = derivatives_2.subList(0, 100).stream()
+					.mapToDouble(a -> a)
+					.sum();
+
 			System.out.println("100 runs: " + derivatives_100 / 100);
+			System.out.println("100_2 runs: " + derivatives_100_2 / 100);
+
 			double derivatives_1000 = derivatives.subList(0, 1000).stream()
 					.mapToDouble(a -> a)
 					.sum();
+
+			double derivatives_1000_2 = derivatives_2.subList(0, 1000).stream()
+					.mapToDouble(a -> a)
+					.sum();
+
 			System.out.println("1000 runs: " + derivatives_1000 / 1000);
+			System.out.println("1000_2 runs: " + derivatives_1000_2 / 1000);
+
 			double derivatives_10000 = derivatives.subList(0, 10000).stream()
 					.mapToDouble(a -> a)
 					.sum();
-			System.out.println("1000 runs: " + derivatives_10000 / 10000);
+
+			double derivatives_10000_2 = derivatives_2.subList(0, 10000).stream()
+					.mapToDouble(a -> a)
+					.sum();
+
+			System.out.println("10000 runs: " + derivatives_10000 / 10000);
+			System.out.println("10000_2 runs: " + derivatives_10000_2 / 10000);
+
 			double derivatives_100000 = derivatives.subList(0, 100000).stream()
 					.mapToDouble(a -> a)
 					.sum();
-			System.out.println("10000 runs: " + derivatives_100000 / 100000);
+			double derivatives_100000_2 = derivatives_2.subList(0, 100000).stream()
+					.mapToDouble(a -> a)
+					.sum();
+
+			System.out.println("100000 runs: " + derivatives_100000 / 100000);
+			System.out.println("100000_2 runs: " + derivatives_100000_2 / 100000);
+
+			double derivatives_1000000 = derivatives.subList(0, 1000000).stream()
+					.mapToDouble(a -> a)
+					.sum();
+			System.out.println("1000000 runs: " + derivatives_1000000 / 1000000);
 
 			return derivatives;
 		}
@@ -317,11 +465,13 @@ public class LikelihoodTest {
 					* gUp(prevEvent.lineages, event.lineages, tau, Ne)
 					* lambda * P_0(event.time);
 
-			return Math.log(ans);
+
+			return ans;
+//			return Math.log(ans);
 		}
 
 		private double logMultif(GeneTreeEvent event, GeneTreeEvent prevEvent, double Ne) {
-			double ans = 0.0;
+			double ans;
 			double mult = 1.0;
 			int sum = event.multiCoalSize.stream().mapToInt(Integer::intValue)
 					.sum();
@@ -332,15 +482,16 @@ public class LikelihoodTest {
 				mult *= binomialInt(sum - n_histories, event.multiCoalSize.get(s) - 1);
 				sum -= (event.multiCoalSize.get(s) - 1);
 			}
-			ans += (1.0 / waysToCoal(prevEvent.lineages, event.lineages)) * mult
+			ans = (1.0 / waysToCoal(prevEvent.lineages, event.lineages)) * mult
 					* gUp(prevEvent.lineages, event.lineages, tau, Ne)
 					* lambda * P_0(event.time);
 
-			return Math.log(ans);
+			return ans;
+//			return Math.log(ans);
 		}
 
 		private double logTrans(GeneTreeEvent event, GeneTreeEvent prevEvent, double Ne) {
-			double ans = 0.0;
+			double ans;
 			double mult = 1.0;
 			int sum = event.multiCoalSize.stream().mapToInt(Integer::intValue)
 					.sum();
@@ -351,10 +502,11 @@ public class LikelihoodTest {
 				mult *= binomialInt(sum - n_histories, event.multiCoalSize.get(s) - 1);
 				sum -= (event.multiCoalSize.get(s) - 1);
 			}
-			ans += (1.0 / waysToCoal(prevEvent.lineages, event.lineages)) * mult
+			ans = (1.0 / waysToCoal(prevEvent.lineages, event.lineages)) * mult
 					* gUp(prevEvent.lineages, event.lineages, tau, Ne);
 
-			return Math.log(ans);
+			return ans;
+//			return Math.log(ans);
 		}
 
 		private double logInterval(GeneTreeEvent event, GeneTreeEvent prevEvent, double Ne) {
@@ -370,7 +522,8 @@ public class LikelihoodTest {
 			ans -= sum * lambda
 					* integralP_0(prevEvent.time, event.time, lambda, mu, psi);
 
-			return ans;
+			return Math.exp(ans);
+//			return ans;
 		}
 
 		private double gUp(int i, int j, double tau, double Ne) {
@@ -383,6 +536,71 @@ public class LikelihoodTest {
 			
 			return ans;
 		}
+
+		private double derivativeNeLogMulti(GeneTreeEvent event, GeneTreeEvent prevEvent) {
+			int i = prevEvent.lineages;
+			int j = event.lineages;
+			double up = 0.0;
+			double down = 0.0;
+			for (int k = j; k <= i; k++) {
+				double coef = (((2 * k - 1) * Math.pow(-1, k - j) * f_1(j, k - 1) * f_2(i, k)) /
+						(MathUtils.factorial(j) * MathUtils.factorial(k - j) * f_1(i, k)));
+				up += coef * (k * (k - 1) / (2 * Math.pow(Ne, 2))) * Math.exp(-(k * (k - 1) * tau * 0.5) / Ne);
+				down += coef * Math.exp(-(k * (k - 1) * tau * 0.5) / Ne);
+			}
+
+			return up / down;
+		}
+
+		private double derivativeNeLogBif(GeneTreeEvent event, GeneTreeEvent prevEvent) {
+			int i = prevEvent.lineages;
+			int j = event.lineages;
+			double up = 0.0;
+			double down = 0.0;
+			for (int k = j; k <= i; k++) {
+				double coef = (((2 * k - 1) * Math.pow(-1, k - j) * f_1(j, k - 1) * f_2(i, k)) /
+						(MathUtils.factorial(j) * MathUtils.factorial(k - j) * f_1(i, k)));
+				up += coef * (k * (k - 1) / (2 * Math.pow(Ne, 2))) * Math.exp(-(k * (k - 1) * tau * 0.5) / Ne);
+				down += coef * Math.exp(-(k * (k - 1) * tau * 0.5) / Ne);
+			}
+			
+
+			double mult = 1.0;
+			int sum = event.multiCoalSize.stream().mapToInt(Integer::intValue)
+					.sum();
+			int n_histories = event.multiCoalSize.size();
+			for (int s = 0; s < n_histories; s++) {
+				mult *= waysToCoal(event.multiCoalSize.get(s), 1);
+				// W factor from NOAH A. ROSENBERG
+				mult *= binomialInt(sum - n_histories, event.multiCoalSize.get(s) - 1);
+				sum -= (event.multiCoalSize.get(s) - 1);
+			}
+			
+			up = ((-1 / Math.pow(Ne, 2))
+					+ ((1.0 / waysToCoal(prevEvent.lineages, event.lineages)) * mult * lambda * P_0(event.time) *
+							up));
+
+			down = ((1 / Ne) +
+					((1.0 / waysToCoal(prevEvent.lineages, event.lineages)) * mult * lambda * P_0(event.time) *
+							down));
+
+			return up / down;
+		}
+
+		private double derivativeNeLogInt(GeneTreeEvent event, GeneTreeEvent prevEvent) {
+			int l = prevEvent.lineages;
+			double t_1 = event.time;
+			double t_0 = prevEvent.time;
+			
+			double ans = ((l * (l - 1)) * (t_1 - t_0) / (2 * Math.pow(Ne, 2)))
+					+ (lambda * integralP_0(prevEvent.time, event.time, lambda, mu, psi)
+							* ((2 * l - 1) * f_1(l, l - 1) * f_2(l, l) * l * (l - 1) * tau
+									* Math.exp(-(l * (l - 1) * tau * 0.5) / Ne))
+							/ (2 * MathUtils.factorial(l) * f_1(l, l) * Math.pow(Ne, 2)));
+			
+			return ans;
+		}
+
 
 		private double P_0(double t) {
 
