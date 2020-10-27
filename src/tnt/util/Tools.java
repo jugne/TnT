3c75e51b59ff7ff4d2ff892743cd59727d147cb5
Ugne Stolz <jugne@bs-mbpr364.d.ethz.ch>
2020-10-27 09:40:56 +0100

    Update utils

--- src/tnt/util/Tools.java
+++ src/tnt/util/Tools.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import beast.evolution.tree.Node;
+import starbeast2.SpeciesTreeInterface;
 
 public class Tools {
 
@@ -26,7 +27,43 @@ public class Tools {
 		return tmp;
 	}
 
+	public static List<Node> getGeneNodesWithParentsAtTransmission(List<Node> nodeList,
+			List<Double> transmissionHeights) {
+		List<Node> tmp = new ArrayList<>();
+		for (Node n : nodeList) {
+			if (n.isRoot())
+				tmp.add(n);
+			else if (transmissionHeights.contains(n.getParent().getHeight()))
+				tmp.add(n);
+		}
+
+		return tmp;
+	}
+
 	public static <T> boolean listEqualsIgnoreOrder(List<T> list1, List<T> list2) {
 		return new HashSet<>(list1).equals(new HashSet<>(list2));
+	}
+
+	public static List<Double> getTransmissionHeights(SpeciesTreeInterface transmissionTree) {
+		List<Double> trHeights = new ArrayList<>();
+		for (Node trNode : transmissionTree.getInternalNodes()) {
+			if (!trNode.isFake()) {
+				trHeights.add(trNode.getHeight());
+			}
+		}
+		return trHeights;
+	}
+
+	public static void getRecipients(Node trTreeSubRoot, List<Node> recipients) {
+		if (trTreeSubRoot.isLeaf())
+			return;
+		Node leftChild = trTreeSubRoot.getChild(0);
+		getRecipients(leftChild, recipients);
+		if (trTreeSubRoot.getChildCount() > 1 && !trTreeSubRoot.isFake()) {
+			Node rightChild = trTreeSubRoot.getChild(1);
+			recipients.add(rightChild);
+			getRecipients(rightChild, recipients);
+		}
+		return;
 	}
 }
