a741c37a2196e75f6f687b899161b6a8965c5aa7
jugne <ugne.stolz@protonmail.com>
2021-05-17 10:41:02 +0200

    Fix taxon order bug

--- src/tnt/transmissionTree/TransmissionTree.java
+++ src/tnt/transmissionTree/TransmissionTree.java
@@ -76,7 +76,7 @@ public class TransmissionTree extends SpeciesTree implements SpeciesTreeInterfac
 	private void orientateNodeChildren(int subtreeRootNr) {
 		Node subTreeRoot = this.getNode(subtreeRootNr);
 		if (!subTreeRoot.isLeaf()) {
-			if (!subTreeRoot.getLeft().metaDataString.equals("orientation=donor")
+			if (!subTreeRoot.getLeft().metaDataString.contains("orientation=donor")
 					|| (subTreeRoot.isFake() && subTreeRoot.getRight().getHeight() != subTreeRoot.getHeight())) {
 				Node left = subTreeRoot.getRight();
 				Node right = subTreeRoot.getLeft();
